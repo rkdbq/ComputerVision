fca5fbc67cea0f3ac6bfce25e19479c261e83393
Bonggu Kang <rkdbg11752@naver.com>
2023-06-01 01:15:16 +0900

    v1

--- omg_trainer/omg_trainer.py
+++ omg_trainer/omg_trainer.py
@@ -1,16 +1,25 @@
-import cv2
+import cv2, pygame, time, threading
+import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QPushButton, QFileDialog, QSlider
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import Qt, QTimer
-from preprocess import frame_with_landmark, webcam_frame_with_landmark
+from preprocess import *
 
 class VideoPlayer(QMainWindow):
     def __init__(self):
         super().__init__()
 
         # 윈도우 설정
-        self.setWindowTitle("Video Player")
-        self.setGeometry(200, 200, 1600, 600)
+        self.setWindowTitle("OMG Trainer")
+        self.setGeometry(200, 200, 1400, 600)
+
+        self.og_title_label = QLabel()
+        self.webcam_title_label = QLabel()
+
+        # 점수
+        self.score_label = QLabel()
+        self.score = 0
+        self.total_score = np.zeros(0)
 
         # 비디오 레이블 초기화
         self.video_label = QLabel()
@@ -21,6 +30,9 @@ class VideoPlayer(QMainWindow):
         self.webcam_timer.timeout.connect(self.update_webcam_frame)
         self.webcam_timer.start(30)
         self.webcam_timestamp = 0
+        self.webcam_landmarks = []
+
+        self.og_timestamp = 0
 
         # 비디오 재생을 위한 타이머 설정
         self.timer = QTimer()
@@ -34,18 +46,52 @@ class VideoPlayer(QMainWindow):
         self.speed_slider = QSlider(Qt.Horizontal)
         self.speed_slider.setMinimum(1)
         self.speed_slider.setMaximum(10)
-        self.speed_slider.setValue(5)
+        self.speed_slider.setValue(10)
         self.speed_slider.valueChanged.connect(self.update_speed)
 
+        og_title_layout = QHBoxLayout()
+        og_title_layout.addWidget(self.og_title_label)
+        og_title_layout.setAlignment(Qt.AlignHCenter)
+
+        webcam_title_layout = QHBoxLayout()
+        webcam_title_layout.addWidget(self.webcam_title_label)
+        webcam_title_layout.setAlignment(Qt.AlignHCenter)
+
+        title_layout = QHBoxLayout()
+        title_layout.addLayout(og_title_layout)
+        title_layout.addLayout(webcam_title_layout)
+
+
         # 레이아웃 초기화
+        score_layout = QVBoxLayout()
+        score_layout.addWidget(self.score_label)
+        score_layout.setAlignment(Qt.AlignHCenter)  # 좌우 가운데 정렬
+    
+        
+        og_control_layout = QVBoxLayout()
+        og_control_layout.addWidget(self.select_button)
+        og_control_layout.addWidget(self.speed_slider)
+        og_control_layout.setContentsMargins(0, 0, 700, 0) 
+
+        og_layout = QVBoxLayout()
+        og_layout.addWidget(self.video_label)
+        og_layout.setAlignment(Qt.AlignHCenter)
+
+        webcam_layout = QVBoxLayout()
+        webcam_layout.addWidget(self.webcam_label)
+        webcam_layout.setAlignment(Qt.AlignHCenter)
+
         video_layout = QHBoxLayout()
-        video_layout.addWidget(self.video_label)
-        video_layout.addWidget(self.webcam_label)
+        video_layout.addLayout(og_layout)
+        video_layout.addLayout(webcam_layout)
+        # video_layout.setAlignment(Qt.AlignHCenter)
+        
 
         layout = QVBoxLayout()
+        layout.addLayout(score_layout)
+        layout.addLayout(title_layout)
         layout.addLayout(video_layout)
-        layout.addWidget(self.select_button)
-        layout.addWidget(self.speed_slider)
+        layout.addLayout(og_control_layout)
 
         # 위젯 설정
         widget = QWidget()
@@ -54,9 +100,18 @@ class VideoPlayer(QMainWindow):
 
         # 선택한 비디오 파일 경로
         self.video_path = ""
+        self.audio_path = ""
 
         # 재생 속도
         self.playback_speed = 1.0
+
+        self.update_score_label("댄스 동영상을 선택하고, 얼마나 비슷하게 출 수 있는지 점수를 측정해보세요.")
+        self.og_title_label.setText("도전할 동영상")
+        self.webcam_title_label.setText("ME")
+
+    def restart_video(self):
+        self.score = 0
+        self.total_score = np.zeros(0)
 
     def select_video(self):
         options = QFileDialog.Options()
@@ -65,9 +120,12 @@ class VideoPlayer(QMainWindow):
         file_dialog.setNameFilter("Video Files (*.mp4 *.avi)")
         file_dialog.setFileMode(QFileDialog.ExistingFile)
         if file_dialog.exec_():
+            self.restart_video()
             selected_files = file_dialog.selectedFiles()
             self.video_path = selected_files[0]
-
+            self.audio_path = change_extension(self.video_path, ".mp3")
+            convert_to_mp3(self.video_path, self.audio_path)
+            
             # 선택한 비디오 파일 열기
             self.cap = cv2.VideoCapture(self.video_path)
 
@@ -92,7 +150,7 @@ class VideoPlayer(QMainWindow):
         # 프레임을 QImage로 변환
         cam_frame = cv2.cvtColor(cam_frame, cv2.COLOR_BGR2RGB)
         cam_frame = cv2.flip(cam_frame, 1)
-        landmarked_frame = webcam_frame_with_landmark(cam_frame, self.webcam_timestamp)
+        landmarked_frame, self.webcam_landmarks = webcam_frame_with_landmark(cam_frame, self.webcam_timestamp)
         
         height, width, channels = landmarked_frame.shape
         q_image = QImage(
@@ -105,17 +163,23 @@ class VideoPlayer(QMainWindow):
 
     def update_frame(self):
         # 비디오 프레임 읽기
-        timestamp = int(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
+        # timestamp = int(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
+        self.og_timestamp += 1
         ret, og_frame = self.cap.read()
 
         # 프레임 읽기에 실패하면 종료
         if not ret:
             self.timer.stop()
+            self.update_score_label(f"Total Score: {round(np.mean(self.total_score), 1)}")
             return
 
         # 프레임을 QImage로 변환
         og_frame = cv2.cvtColor(og_frame, cv2.COLOR_BGR2RGB)
-        landmarked_frame = frame_with_landmark(og_frame, timestamp)
+        landmarked_frame, og_landmarks = frame_with_landmark(og_frame, self.og_timestamp)
+        self.score = (1 - calculate_landmark_loss(og_landmarks, self.webcam_landmarks)) * 100
+        self.total_score = np.concatenate((self.total_score, np.array([self.score])), axis=0)
+        self.update_score_label()
+        # print(self.score)
 
         height, width, channels = landmarked_frame.shape
         q_image = QImage(
@@ -127,10 +191,35 @@ class VideoPlayer(QMainWindow):
         self.video_label.setPixmap(pixmap.scaledToWidth(600, Qt.SmoothTransformation))
 
         # 재생 속도 적용
-        delay = int(1000 / (self.cap.get(cv2.CAP_PROP_FPS) * self.playback_speed))
+        delay = int((1000 / (self.cap.get(cv2.CAP_PROP_FPS) * self.playback_speed)))
+        if self.playback_speed == 2.0:
+            delay = 0
+        # print(self.playback_speed)
+        # print(delay)
 
         # 딜레이 후 타이머 재실행
         self.timer.start(delay)
+
+    def update_score_label(self, text=""):
+        # score_label에 현재 score 값을 표시
+        bad = "font-size: 20px; color: red;"
+        good = "font-size: 20px; color: orange;"
+        excellent = "font-size: 20px; color: green;"
+
+        if text != "":
+            self.score_label.setStyleSheet("font-size: 20px;")
+            self.score_label.setText(text)
+            return
+
+        if self.score < 60:
+            self.score_label.setStyleSheet(bad)
+            self.score_label.setText(f"Try Hard ({round(self.score, 1)}% simular)") 
+        elif self.score < 85:
+            self.score_label.setStyleSheet(good)
+            self.score_label.setText(f"Keep Going ({round(self.score, 1)}% simular)")
+        else:
+            self.score_label.setStyleSheet(excellent)
+            self.score_label.setText(f"Perfect ({round(self.score, 1)}% simular)")   
 
 
 if __name__ == "__main__":
--- omg_trainer/preprocess.py
+++ omg_trainer/preprocess.py
@@ -3,6 +3,8 @@ import numpy as np
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 
+presence_threshold = 0.5
+
 BaseOptions = mp.tasks.BaseOptions
 PoseLandmarker = mp.tasks.vision.PoseLandmarker
 PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
@@ -33,7 +35,7 @@ def draw_landmarks_on_image(rgb_image, detection_result):
   return annotated_image
 
 prac_landmarker_result = None
-def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+def get_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     global prac_landmarker_result
     prac_landmarker_result = result
 
@@ -44,7 +46,7 @@ og_options = PoseLandmarkerOptions(
 prac_options = PoseLandmarkerOptions(
     base_options=BaseOptions(model_asset_path='omg_trainer//pose_landmarker_lite.task'),
     running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
+    result_callback=get_result)
 
 og_landmarker = PoseLandmarker.create_from_options(og_options)
 prac_landmarker = PoseLandmarker.create_from_options(prac_options)
@@ -53,10 +55,84 @@ def frame_with_landmark(frame, timestamp):
    og_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
    og_landmarker_result = og_landmarker.detect_for_video(og_image, timestamp)
 
-   return draw_landmarks_on_image(og_image.numpy_view(), og_landmarker_result)
+   return draw_landmarks_on_image(og_image.numpy_view(), og_landmarker_result), og_landmarker_result
 
 def webcam_frame_with_landmark(frame, timestamp):
     prac_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
     prac_landmarker.detect_async(prac_image, timestamp)
 
-    return draw_landmarks_on_image(prac_image.numpy_view(), prac_landmarker_result)
+    return draw_landmarks_on_image(prac_image.numpy_view(), prac_landmarker_result), prac_landmarker_result
+
+def calculate_landmark_loss(og_landmarker_result, prac_landmarker_result):
+   og_len = len(og_landmarker_result.pose_landmarks)
+   prac_len = len(prac_landmarker_result.pose_landmarks)
+   loss = np.zeros(0)
+   if og_len == 0:
+      if prac_len == 0:
+         loss = 0
+      else:
+         prac_pose_landmarks = prac_landmarker_result.pose_landmarks[0]
+         prac_pose_landmarks = prac_pose_landmarks[:1] + prac_pose_landmarks[7:9] + prac_pose_landmarks[11:]
+         for landmark in prac_pose_landmarks:
+            landmark_array = np.ones(4)
+            if landmark.presence < presence_threshold:
+                landmark_array = np.zeros(4)
+            loss = np.concatenate((loss, landmark_array**2), axis=0)
+         loss = np.mean(loss)
+   else:
+      if prac_len == 0:
+         og_pose_landmarks = og_landmarker_result.pose_landmarks[0]
+         og_pose_landmarks = og_pose_landmarks[:1] + og_pose_landmarks[7:9] + og_pose_landmarks[11:]
+         for landmark in og_pose_landmarks:
+            landmark_array = np.ones(4)
+            if landmark.presence < presence_threshold:
+                landmark_array = np.zeros(4)
+            loss = np.concatenate((loss, landmark_array**2), axis=0)
+         loss = np.mean(loss)
+
+      else:
+         og_pose_landmarks = og_landmarker_result.pose_landmarks[0]
+         prac_pose_landmarks = prac_landmarker_result.pose_landmarks[0]
+         og_pose_landmarks = og_pose_landmarks[:1] + og_pose_landmarks[7:9] + og_pose_landmarks[11:]
+         prac_pose_landmarks = prac_pose_landmarks[:1] + prac_pose_landmarks[7:9] + prac_pose_landmarks[11:]
+         for og_landmark, prac_landmark in zip(og_pose_landmarks, prac_pose_landmarks):
+            og_landmark_array = normalize(np.array([og_landmark.x, og_landmark.y, og_landmark.z, og_landmark.visibility]))
+            prac_landmark_array = normalize(np.array([prac_landmark.x, prac_landmark.y, prac_landmark.z, prac_landmark.visibility]))
+            if og_landmark.presence < presence_threshold:
+                og_landmark_array = prac_landmark_array - 1
+            if prac_landmark.presence < presence_threshold:
+               prac_landmark_array = og_landmark_array - 1
+            loss = np.concatenate((loss, ((og_landmark_array) - prac_landmark_array)**2), axis=0)
+         loss = np.mean(loss)
+
+   return loss
+
+def normalize(array):
+   min_val = np.min(array)
+   max_val = np.max(array)
+   normalized_arr = (array - min_val) / (max_val - min_val)
+
+   return normalized_arr
+
+import subprocess
+
+def convert_to_mp3(input_file, output_file):
+    if os.path.exists(output_file):
+       return
+
+    # FFmpeg 명령어 설정
+    command = ['ffmpeg', '-i', input_file, '-vn', '-acodec', 'libmp3lame', '-ab', '192k', '-f', 'mp3', output_file]
+
+    # FFmpeg 실행
+    subprocess.call(command)
+
+import os
+
+def change_extension(file_path, new_extension):
+    # 기존 파일 경로에서 확장자 추출
+    base_path, _ = os.path.splitext(file_path)
+
+    # 새로운 확장자를 추가하여 파일 경로 생성
+    new_file_path = base_path + new_extension
+
+    return new_file_path    
